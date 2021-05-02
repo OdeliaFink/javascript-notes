214bbb2ae384bed5d5a344f0aaa821c3692f493b
OdeliaFink <odelia.finkel@gmail.com>
2021-05-02 09:40:49 -0400

    Initial commit

---
+++ shortcuts.js
@@ -0,0 +1,10 @@
+//SEE IF THERES AN EXISTING SPACE:
+
+let password = 'I am Odelia';
+
+password.indexOf(' ') - 1; //this would show if there isn't a space
+
+//ARRAYS: if we want to see the last item
+
+let colors = ['blue', 'purple', 'green', 'red', 'violet'];
+colors[colors.length - 1];
