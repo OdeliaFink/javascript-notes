7c7e95011c29be447a383d906860382d3c3434c9
OdeliaFink <odelia.finkel@gmail.com>
2021-05-02 10:16:35 -0400

    Added Concat and indexOf shortcuts

--- shortcuts.js
+++ shortcuts.js
@@ -8,3 +8,11 @@ password.indexOf(' ') - 1; //this would show if there isn't a space
 
 let colors = ['blue', 'purple', 'green', 'red', 'violet'];
 colors[colors.length - 1];
+
+//CONCAT
+
+let array1 = ['apples', 'tomatoes', 'potatoes'];
+let array2 = ['carrot', 'ginger'];
+
+console.log(array2.concat(array1));
+//Concat can do for multiple arrays just add it within concat()
