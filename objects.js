693ff7f4ce626d7de4d8234d99daa693d0960bb6
OdeliaFink <odelia.finkel@gmail.com>
2021-05-04 18:50:17 -0400

    Notes on Objects

---
+++ objects.js
@@ -0,0 +1,17 @@
+//OBJECT:
+
+//All keys are strings
+//Can access with . or []
+
+let userReview = {};
+//To add you access property and assign
+userReview.odelia = 3.5;
+
+const user = {
+  username: 'odelia',
+  email: 'example@abc.com',
+  notification: ['message'],
+};
+
+//figure out how many notification
+user.notifications.length;
