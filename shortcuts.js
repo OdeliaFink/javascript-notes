1a8b2dc7816d55aa5c5ddbc6aafd91803919d275
OdeliaFink <odelia.finkel@gmail.com>
2021-05-03 21:46:26 -0400

    Added notes on includes

--- shortcuts.js
+++ shortcuts.js
@@ -1,7 +1,6 @@
 //SEE IF THERES AN EXISTING SPACE:
 
 let password = 'I am Odelia';
-
 password.indexOf(' ') - 1; //this would show if there isn't a space
 
 //ARRAYS: if we want to see the last item
@@ -9,10 +8,13 @@ password.indexOf(' ') - 1; //this would show if there isn't a space
 let colors = ['blue', 'purple', 'green', 'red', 'violet'];
 colors[colors.length - 1];
 
-//CONCAT
+//CONCAT: can do for multiple arrays just add it within concat()
 
 let array1 = ['apples', 'tomatoes', 'potatoes'];
 let array2 = ['carrot', 'ginger'];
 
 console.log(array2.concat(array1));
-//Concat can do for multiple arrays just add it within concat()
+
+//INCLUDES:
+let colors = ['blue', 'purple', 'green', 'red', 'violet'];
+colors.includes('fish'); //would return false
