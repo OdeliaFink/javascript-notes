48b14dde853294538eebfc35e9997d8b8bd1e864
OdeliaFink <odelia.finkel@gmail.com>
2021-05-09 19:44:34 -0400

    Object destructuring

--- functions/spread/destructuring.js
+++ functions/spread/destructuring.js
@@ -9,3 +9,24 @@ const raceResults = [
 
 const [first, second, third] = raceResults;
 console.log(third);
+
+//OBJECT DESTRUCTURING
+//make variable based off the name of the property
+//use property name to unpack
+
+const runner = {
+  first: 'Eliud',
+  last: 'Kipchoge',
+  country: 'Kenya',
+  title: 'Elder of the Order of the Golden Heart of Kenya',
+};
+
+// const {
+//   first,
+//   last,
+//   time
+// } = runner;
+
+const { country: nation, title: honorific } = runner;
+
+const { first, last, ...other } = runner;
