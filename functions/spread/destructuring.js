d11c86f4b63f6db7cc143dba3f29ec497a1c8991
OdeliaFink <odelia.finkel@gmail.com>
2021-05-09 14:14:02 -0400

    Added notes on destructuring

---
+++ functions/spread/destructuring.js
@@ -0,0 +1,11 @@
+const raceResults = [
+  'Eliud Kipchoge',
+  'Feyisa Lelisa',
+  'Galen Rupp',
+  'Ghirmay Ghebreslassie',
+  'Alphonce Simbu',
+  'Jared Ward',
+];
+
+const [first, second, third] = raceResults;
+console.log(third);
