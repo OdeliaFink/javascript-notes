6a1ba01d7dddcf25f35e94da709179479ca7f98c
OdeliaFink <odelia.finkel@gmail.com>
2021-05-19 19:58:24 -0400

    Adding values in resolve or reject

--- functions/DOM/promises2.js
+++ functions/DOM/promises2.js
@@ -1,10 +1,28 @@
+// const willGetYouADog = new Promise((resolve, reject) => {
+//   const rand = Math.random();
+//   if (rand < 0.5) {
+//     resolve();
+//   } else {
+//     reject();
+//   }
+// });
+
+// willGetYouADog.then(() => {
+//   console.log('Yay we got a dog');
+// }); //run if reolved()
+// willGetYouADog.catch(() => {
+//   console.log('NO DOG');
+// });
+
 const willGetYouADog = new Promise((resolve, reject) => {
-  const rand = Math.random();
-  if (rand < 0.5) {
-    resolve();
-  } else {
-    reject();
-  }
+  setTimeout(() => {
+    const rand = Math.random();
+    if (rand < 0.5) {
+      resolve();
+    } else {
+      reject();
+    }
+  }, 5000);
 });
 
 willGetYouADog.then(() => {
