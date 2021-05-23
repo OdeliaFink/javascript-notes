bc8e474537439c9e2e801035aa14b352bc3624fd
OdeliaFink <odelia.finkel@gmail.com>
2021-05-22 22:45:54 -0400

    Chained axios request

--- axios.js
+++ axios.js
@@ -1,12 +1,30 @@
 //axios is an extrernal library that uses fetch behind the scenes.
 const axios = require('axios');
 //can change request to post or put
+// axios
+//   .get('https://swapi.dev/api/planets')
+//   .then((res) => {
+//     console.log(res.data);
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//     console.log('IN CATCH');
+//   });
+
+//chaining axious:
+
 axios
   .get('https://swapi.dev/api/planets')
-  .then((res) => {
-    console.log(res.data);
+  .then(({ data }) => {
+    console.log(data);
+    for (let planet of data.results) {
+      console.log(planet.name);
+    }
+    return axios.get(data.next);
   })
-  .catch((err) => {
-    console.log(err);
-    console.log('IN CATCH');
+  .then(({ data }) => {
+    console.log(data);
+    for (let planet of data.results) {
+      console.log(planet.name);
+    }
   });
