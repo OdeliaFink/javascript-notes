ec6c57f54d6fd65831259aa6e3ac08dcd4f50369
OdeliaFink <odelia.finkel@gmail.com>
2021-05-22 22:51:50 -0400

    Updated chained axios

--- axios.js
+++ axios.js
@@ -13,18 +13,25 @@ const axios = require('axios');
 
 //chaining axious:
 
-axios
-  .get('https://swapi.dev/api/planets')
-  .then(({ data }) => {
-    console.log(data);
-    for (let planet of data.results) {
-      console.log(planet.name);
-    }
-    return axios.get(data.next);
-  })
-  .then(({ data }) => {
-    console.log(data);
-    for (let planet of data.results) {
-      console.log(planet.name);
-    }
+const fetchNextPlanets = (url = 'https://swapi.dev/api/planets') => {
+  return axios.get(url);
+};
+
+const printPlanets = ({ data }) => {
+  console.log(data);
+  for (let planet of data.results) {
+    console.log(planet.name);
+  }
+  return Promise.resolve(data.next);
+};
+
+fetchNextPlanets()
+  .then(printPlanets)
+  .then(fetchNextPlanets)
+  .then(printPlanets)
+  .then(fetchNextPlanets)
+  .then(printPlanets)
+  .catch((err) => {
+    console.log(err);
+    console.log('IN CATCH');
   });
